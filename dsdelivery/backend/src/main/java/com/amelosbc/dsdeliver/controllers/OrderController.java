ee8643eae23663928a6bafa035f34d6dd1d5e7f5
amelosbc <alexandremelo52@gmail.com>
2021-01-06 00:50:57 -0300

    Criação dos Endpoints POST order

--- dsdelivery/backend/src/main/java/com/amelosbc/dsdeliver/controllers/OrderController.java
+++ dsdelivery/backend/src/main/java/com/amelosbc/dsdeliver/controllers/OrderController.java
@@ -1,13 +1,21 @@
 package com.amelosbc.dsdeliver.controllers;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.amelosbc.dsdeliver.entities.Order;
+import com.amelosbc.dsdeliver.entities.OrderStatus;
 import com.amelosbc.dsdeliver.entities.dto.OrderDTO;
 import com.amelosbc.dsdeliver.services.OrderService;
 
@@ -22,5 +30,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List <OrderDTO> list = service.findOrderWithProducts();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert (@RequestBody OrderDTO dto) {
+		dto = service.insert (dto);
+		URI  uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- dsdelivery/backend/src/main/java/com/amelosbc/dsdeliver/services/OrderService.java
+++ dsdelivery/backend/src/main/java/com/amelosbc/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.amelosbc.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.amelosbc.dsdeliver.entities.Order;
+import com.amelosbc.dsdeliver.entities.OrderStatus;
+import com.amelosbc.dsdeliver.entities.Product;
 import com.amelosbc.dsdeliver.entities.dto.OrderDTO;
 import com.amelosbc.dsdeliver.repositories.OrderRepository;
+import com.amelosbc.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +21,25 @@ public class OrderService {
 	@Autowired   // injeção de dependencia via framework spring
 	private OrderRepository repository;
 	
+	@Autowired   // injeção de dependencia via framework spring
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findOrderWithProducts() {
 		List<Order> list = repository.findAll();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert (OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (com.amelosbc.dsdeliver.entities.dto.ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+
 }
