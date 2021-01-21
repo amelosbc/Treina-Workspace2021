e89ce7f1f1613e3bc5b5185fcf7667addccac410
amelosbc <alexandremelo52@gmail.com>
2021-01-20 23:55:27 -0300

    git add .

--- dsdelivery/front-web/src/Footer/index.tsx
+++ dsdelivery/front-web/src/Footer/index.tsx
@@ -1,9 +1,27 @@
 import './styles.css';
-import { ReactComponent as Logo} from './logo.svg';
+import { ReactComponent as YoutubeIcon} from './Youtube.svg';
+import { ReactComponent as InstagramIcon} from './Instagram.svg';
+import { ReactComponent as LinkedinIcon} from './Linkedin.svg';
 
 function Footer() {
     return (
+
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
     )
 }
 
-export default Home;
+export default Footer;
